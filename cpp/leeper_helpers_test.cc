887686ef80a37689516905431e302eebcda138a6
Adam Leeper <adamleeper@gmail.com>
2020-12-08 10:53:11 -0800

    more user friendly makefile

--- cpp/leeper_helpers_test.cc
+++ cpp/leeper_helpers_test.cc
@@ -1,60 +1,21 @@
 #include "leeper_helpers.h"
 
-#include <string>
 #include <vector>
-#include <iomanip>
-#include <iostream>
-#include <algorithm>    // std::remove_if
 
-using std::cout;
-using std::endl;
-using std::vector;
-
-
-
-// bool IsOdd (int i) { return ((i%2)==1); }
-
-void removeIfArray() {
-  int myints[] = {1,2,3,4,5,6,7,8,9};            // 1 2 3 4 5 6 7 8 9
-
-  // bounds of range:
-  int* pbegin = myints;                          // ^
-  int* pend = myints+sizeof(myints)/sizeof(int); // ^                 ^
-
-  pend = std::remove_if(pbegin, pend, [](int i) { return (i%2)==1; });   // 2 4 6 8 ? ? ? ? ?
-                                                 // ^       ^
-  std::cout << "the range contains:";
-  for (int* p=pbegin; p!=pend; ++p) {
-    std::cout << ' ' << *p;
-  }
-  std::cout << '\n';
-}
-
-void removeIfVector() {
-  vector<int> myints = {1,2,3,4,5,6,7,8,9,10};            // 1 2 3 4 5 6 7 8 9
-
-  auto remove_fn = [&](int i) { 
-    cout << "vector is now: " << myints << endl;
-    return (i%2)==1;
-  };
-  auto delete_me = std::remove_if(myints.begin(), myints.end(), remove_fn);
-  cout << "vector ended as: " << myints << endl;
-  myints.erase(delete_me, myints.end());   // 2 4 6 8 ? ? ? ? ?
-                                                 // ^       ^
-  std::cout << "the range contains: " << myints << endl;
-}
 
 int main() {
-  printf("hello world!\n");
-  vector<int> values = {1, 2, 3, 4, 5};
+  using std::cout;
+  using std::endl;
+  using std::vector;
 
-  vector<vector<std::string>> letters = {{"abc", "def"}, {"ghi", "jkl"}};
+  vector<int> one_dim = {1, 2, 3, 4, 5};
 
-  cout << "values:\n" << values << endl;
+  vector<vector<std::string>> two_dim = {{"abc", "def"}, {"ghi", "jkl"}};
 
-  cout << "letters:\n" << letters << endl;
+  cout << "one_dim ints:\n" << one_dim << endl;
+  cout << "one_dim strings:\n" << two_dim[0] << endl;
 
-  removeIfArray();
-  removeIfVector();
+  cout << "two_dim strings:\n" << two_dim << endl;
+
   return 0;
 }
---
+++ cpp/remove_if.cc
@@ -0,0 +1,57 @@
+#include "leeper_helpers.h"
+
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>    // std::remove_if
+
+using std::cout;
+using std::endl;
+
+void removeIfArray() {
+  cout << "Running removeIfArray example:" << endl;
+  int myints[] = {1,2,3,4,5,6,7,8,9};            // 1 2 3 4 5 6 7 8 9
+
+  // bounds of range:
+  int* pbegin = myints;                          // ^
+  int* pend = myints+sizeof(myints)/sizeof(int); // ^                 ^
+
+  auto remove_fn = [&](int i) { 
+    cout << "vector is now: ";
+    for (int* p=pbegin; p!=pend; ++p) {
+      std::cout << ' ' << *p;
+    }
+    cout << endl;
+    return (i%2)==1;
+  };
+
+  pend = std::remove_if(pbegin, pend, remove_fn);   // 2 4 6 8 ? ? ? ? ?
+                                                 // ^       ^
+  std::cout << "the range contains:";
+  for (int* p=pbegin; p!=pend; ++p) {
+    std::cout << ' ' << *p;
+  }
+  std::cout << '\n';
+}
+
+void removeIfVector() {
+  cout << "Running removeIfVector example:" << endl;
+  vector<int> myints = {1,2,3,4,5,6,7,8,9,10};            // 1 2 3 4 5 6 7 8 9
+
+  auto remove_fn = [&](int i) { 
+    cout << "vector is now: " << myints << endl;
+    return (i%2)==1;
+  };
+  auto delete_me = std::remove_if(myints.begin(), myints.end(), remove_fn);
+  cout << "vector ended as: " << myints << endl;
+  myints.erase(delete_me, myints.end());   // 2 4 6 8 ? ? ? ? ?
+                                                 // ^       ^
+  std::cout << "the range contains: " << myints << endl;
+}
+
+int main() {
+  removeIfArray();
+  removeIfVector();
+  return 0;
+}
